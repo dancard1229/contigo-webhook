7271570981e9fde4b73c164ca37627dc7899bb09
dancard1229 <dan.card1229@gmail.com>
2026-04-03 12:23:50 -0400

    Update get-appointments.js

--- api/get-appointments.js
+++ api/get-appointments.js
@@ -22,8 +22,7 @@ module.exports = async (req, res) => {
     const userUri = userData.resource && userData.resource.uri;
 
     const eventsRes = await fetch(
- "https://api.calendly.com/scheduled_events?organization=" + encodeURIComponent(orgUri) + (email.toLowerCase() !== "dan.card1229@gmail.com" ? "&invitee_email=" + encodeURIComponent(email) : "") +
-      { headers: { Authorization: "Bearer " + CALENDLY_TOKEN } }
+   "https://api.calendly.com/scheduled_events?organization=" + encodeURIComponent(orgUri) + (email.toLowerCase() !== "dan.card1229@gmail.com" ? "&invitee_email=" + encodeURIComponent(email) : "") + "&status=active&count=100",
     );
     const eventsData = await eventsRes.json();
     const allEvents = eventsData.collection || [];
